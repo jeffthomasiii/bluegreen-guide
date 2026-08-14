f8453814d71499e4bcd0e2daf76c4408dde60477
jeffthomasiii <Masterstudent@duck.com>
2026-08-14 16:24:56 -0700

    Generate Mission Bay canonical browser data

--- scripts/build-launch-data-js.js
+++ scripts/build-launch-data-js.js
@@ -1,12 +1,21 @@
 const fs = require("fs");
 const path = require("path");
 
-const dataPath = path.join(__dirname, "..", "data", "launch-points.json");
-const outputPath = path.join(__dirname, "..", "data", "launch-points.js");
+const dataDir = path.join(__dirname, "..", "data");
+const baseJsonPath = path.join(dataDir, "launch-points.json");
+const missionBayJsonPath = path.join(dataDir, "mission-bay-launch-points.json");
+const baseOutputPath = path.join(dataDir, "launch-points.js");
+const missionBayOutputPath = path.join(dataDir, "mission-bay-launch-points.js");
 
-const json = fs.readFileSync(dataPath, "utf8");
-const data = JSON.parse(json);
+const baseData = JSON.parse(fs.readFileSync(baseJsonPath, "utf8"));
+const missionBayData = JSON.parse(fs.readFileSync(missionBayJsonPath, "utf8"));
 
-fs.writeFileSync(outputPath, `window.LAUNCH_POINTS = ${JSON.stringify(data, null, 2)};\n`);
+fs.writeFileSync(baseOutputPath, `window.LAUNCH_POINTS = ${JSON.stringify(baseData, null, 2)};\n`);
+fs.writeFileSync(
+  missionBayOutputPath,
+  `window.LAUNCH_POINTS = [...(Array.isArray(window.LAUNCH_POINTS) ? window.LAUNCH_POINTS : []), ...${JSON.stringify(missionBayData, null, 2)}];\n`
+);
 
-console.log(`Wrote ${data.length} launch points to data/launch-points.js`);
+console.log(
+  `Wrote ${baseData.length} base launch points and ${missionBayData.length} Mission Bay launch points (${baseData.length + missionBayData.length} total runtime places).`
+);
