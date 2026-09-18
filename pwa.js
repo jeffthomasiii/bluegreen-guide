bf71e5e7c25aa08dea898c48e2dcaf8b34bdca74
Jeff Thomas III <Masterstudent@duck.com>
2026-09-17 21:07:50 -0700

    Refresh installed PWA when a new shell takes control

--- pwa.js
+++ pwa.js
@@ -1,7 +1,21 @@
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./service-worker.js").catch((error) => {
-      console.warn("BlueGreen Guide offline support could not be enabled.", error);
+  const hadController = Boolean(navigator.serviceWorker.controller);
+  let refreshing = false;
+
+  if (hadController) {
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+      if (refreshing) return;
+      refreshing = true;
+      window.location.reload();
     });
+  }
+
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then((registration) => registration.update())
+      .catch((error) => {
+        console.warn("BlueGreen Guide offline support could not be enabled.", error);
+      });
   });
 }
